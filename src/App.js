b55beed398e269ce1af75002c1d635ba90d3b7ad
eduardsdn <eduard.sdn@gmail.com>
2024-01-16 13:25:08 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+import AppCSS from "./styles/app.module.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>Projects</li>
+          <li>Contact</li>
+          <li>Whatever</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
